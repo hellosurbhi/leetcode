aa5c3a9b240f794e4bc307e5b32892eb504cf601
Surbhi <65139581+hellosurbhi@users.noreply.github.com>
2021-03-10 20:09:16 -0800

    Update integer-to-roman.js

--- integer-to-roman/integer-to-roman.js
+++ integer-to-roman/integer-to-roman.js
@@ -3,14 +3,6 @@
  * @return {string}
  */
 
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
-
 var intToRoman = function(num) {
   const romanCalculator = [
     [1000, 'M'],
