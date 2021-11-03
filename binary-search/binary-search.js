e0da0bcf4e1bc884a5fe55d8340d853ff89e88cd
Surbhi <65139581+hellosurbhi@users.noreply.github.com>
2021-11-03 07:26:49 -0400

    Time: 84 ms (53.63%), Space: 42.6 MB (7.92%) - LeetHub

---
+++ binary-search/binary-search.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2 );
+        let current = nums[mid];
+        
+        if (current === target) {
+            return mid  
+        } else if (target < current ) {
+            end = mid - 1;
+             
+        } else {
+            start = mid + 1
+        }
+    };
+    
+    return -1 
+    
+    
+};
